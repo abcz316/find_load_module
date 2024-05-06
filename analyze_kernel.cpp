aa36fc1504e256a39da5ee35f4734c5efd4449d0
unkn0wName <unkn0wName@gmail.com>
2024-05-07 00:50:30 +0800

    Update symbol V2

---
+++ analyze_kernel.cpp
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "analyze_kernel.h"
+
+AnalyzeKernel::AnalyzeKernel(const std::vector<char>& file_buf) : m_file_buf(file_buf), m_kernel_sym_parser(file_buf)
+{
+}
+
+AnalyzeKernel::~AnalyzeKernel()
+{
+}
+
+bool AnalyzeKernel::analyze_kernel_symbol() {
+	if (!m_kernel_sym_parser.init_kallsyms_lookup_name()) {
+		std::cout << "Failed to initialize kallsyms lookup name" << std::endl;
+		return false;
+	}
+	if (!find_symbol_offset()) {
+		std::cout << "Failed to find symbol offset" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+KernelSymbolOffset AnalyzeKernel::get_symbol_offset() {
+	return m_kernel_sym_offset;
+}
+
+bool AnalyzeKernel::find_symbol_offset() {
+	m_kernel_sym_offset._text_offset = m_kernel_sym_parser.kallsyms_lookup_name("_text");
+	m_kernel_sym_offset._stext_offset = m_kernel_sym_parser.kallsyms_lookup_name("_stext");
+	m_kernel_sym_offset.load_module_offset = m_kernel_sym_parser.kallsyms_lookup_name("load_module");
+	if (m_kernel_sym_offset.load_module_offset == 0) {
+		m_kernel_sym_offset.load_module_offset = m_kernel_sym_parser.kallsyms_lookup_name("load_module", true);
+	}
+	return m_kernel_sym_offset.load_module_offset;
+}
---
+++ analyze_kernel.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "kernel_symbol_parser.h"
+#include <iostream>
+#include <vector>
+
+struct KernelSymbolOffset {
+	size_t _text_offset = 0;
+	size_t _stext_offset = 0;
+	size_t load_module_offset = 0;
+};
+
+class AnalyzeKernel
+{
+public:
+	AnalyzeKernel(const std::vector<char> & file_buf);
+	~AnalyzeKernel();
+
+public:
+	bool analyze_kernel_symbol();
+	KernelSymbolOffset get_symbol_offset();
+private:
+	bool find_symbol_offset();
+	const std::vector<char>& m_file_buf;
+	KernelSymbolParser m_kernel_sym_parser;
+	KernelSymbolOffset m_kernel_sym_offset;
+};
